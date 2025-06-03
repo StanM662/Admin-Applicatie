23fb681bdd07101b368f27be407b59cefcbb0998
Rick <rick.t1302@hotmail.com>
2025-06-03 13:11:17 +0200

    Dashboard begin

---
+++ KE03_INTDEV_SE_2_Base/ViewModel/DashboardViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace KE03_INTDEV_SE_2_Base.ViewModels
+{
+    public class DashboardViewModel
+    {
+        public int TotalCustomers { get; set; }
+        public int TotalOrders { get; set; }
+        public int TotalProducts { get; set; }
+        public int TotalParts { get; set; }
+    }
+}
