0de28a6bd1d92258d410f974ad0f06c37a08e936
StanM662 <2403308morreau@zuyd.nl>
2025-06-05 10:48:13 +0200

    Update navbar

---
+++ KE03_INTDEV_SE_2_Base/Controllers/LogsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KE03_INTDEV_SE_2_Base.Controllers
+{
+    public class LogsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
